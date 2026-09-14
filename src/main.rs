fbec67726d8d25f87655b32b50a8a8460cf3c572
Andreas Kaempf <andreas.kaempf@soundience.com>
2026-09-14 22:45:44 +0200

    Moved query to a separate function that returns Parquet array

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,29 @@
 // Execute query on command line against Parquet files in the data directory
 
+use datafusion::arrow::array::RecordBatch;
+use datafusion::arrow::util::pretty::pretty_format_batches;
+use datafusion::error::Result;
 use datafusion::prelude::*;
-use std::env;
 
 #[tokio::main]
-async fn main() -> datafusion::error::Result<()> {
+async fn main() -> Result<()> {
+    let batches = query("select max(fare_amount) from taxi").await?;
+    println!("{}", pretty_format_batches(&batches)?);
+    Ok(())
+}
 
-    // Get query from command line
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        println!("Missing query");
-        return Ok(());
-    }
-    let q = &args[1];
-    println!("Query: {}", q);
-
+// Execute a query, returning the resulting record batches
+async fn query(q: &str) -> Result<Vec<RecordBatch>> {
     // Register the table
     let ctx = SessionContext::new();
-    ctx.register_parquet("taxi", "data", ParquetReadOptions::new()).await?;
+    ctx.register_parquet("taxi", "data", ParquetReadOptions::new())
+        .await?;
 
     // Create a plan to run a SQL query
     let df = ctx.sql(q).await?;
 
-    // Execute and print results
-    df.show().await?;
-    Ok(())
+    // Execute and collect the results
+    let batches = df.collect().await?;
+
+    Ok(batches)
 }
