92a143905662cfba3f1477a8d30e7d6f057d2867
Andreas Kaempf <andreas.kaempf@soundience.com>
2026-09-15 10:35:23 +0200

    Cleaned up command line argument processing

--- parquet_query/src/main.rs
+++ parquet_query/src/main.rs
@@ -1,4 +1,15 @@
-// Execute query on command line against Parquet files in the data directory
+// Execute query on command line against Parquet files in the data directory,
+// which is assumed to be under the parent directory.
+//
+// Sample usage:  cargo run "select avg(fare_amount) from rides"
+//
+// Sample output:
+// Query: select avg(fare_amount) from rides
+// +------------------------+
+// | avg(rides.fare_amount) |
+// +------------------------+
+// | 19.219927893175566     |
+// +------------------------+
 
 use datafusion::arrow::array::RecordBatch;
 use datafusion::arrow::util::pretty::pretty_format_batches;
@@ -7,15 +18,16 @@ use datafusion::prelude::*;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-
     // Get query from the command line
-    let mut args = std::env::args();
-    let program = args.next().unwrap_or_else(|| "parquet_query".to_string());
-    let Some(q) = args.next() else {
-        eprintln!("Usage: {} \"<query>\"", program);
+    let args: Vec<_> = std::env::args().collect();
+    if args.len() != 2 {
+        println!("Missing query or extra arguments");
         std::process::exit(1);
-    };
+    }
+    let q = args[1].as_str();
+    println!("Query: {}", q);
 
+    // Execute query and show result
     let batches = query(&q).await?;
     println!("{}", pretty_format_batches(&batches)?);
     Ok(())
@@ -25,7 +37,7 @@ async fn main() -> Result<()> {
 async fn query(q: &str) -> Result<Vec<RecordBatch>> {
     // Register the table
     let ctx = SessionContext::new();
-    ctx.register_parquet("taxi", "data", ParquetReadOptions::new())
+    ctx.register_parquet("rides", "../data", ParquetReadOptions::new())
         .await?;
 
     // Create a plan to run a SQL query
